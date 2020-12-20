351f30d3f5544123c62cdfa0dca81264284d915e
briansimiyuj <briansimiyuj@gmail.com>
2020-12-20 11:56:10 +0300

    Next Slide Function

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ const nextBTN = document.querySelector("#next-btn")
 const intervalTime = 5000
 
 let nextInterval 
+
+
+
+const nextSlide = () =>{
+
+       current.classList.remove("current")
+
+
+       if (current.nextElementSibling) {
+           
+            current.nextElementSibling.classList.add("current")
+
+       } else {
+
+            slides[0].classList.add("current")                                
+           
+       }
+
+
+
+       currentBall.classList.remove("current-ball")
+
+
+       if (currentBall.nextElementSibling) {
+           
+            currentBall.nextElementSibling.classList.add("current-ball")
+
+       } else {
+
+            slides[0].classList.add("current-ball")                                
+           
+       }
+
+       console.log('working')
+
+}
+
+nextSlide()
