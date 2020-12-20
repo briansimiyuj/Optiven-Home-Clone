45bb98a0b035e47b5f3f8e3dbcc7dc04d4a6d025
briansimiyuj <briansimiyuj@gmail.com>
2020-12-20 12:48:20 +0300

    Previous Slide Function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const slides = document.querySelectorAll(".slides")
+const slides = document.querySelectorAll(".slide")
 
 const current = document.querySelector(".current")  
 
@@ -16,8 +16,6 @@ const intervalTime = 5000
 
 let nextInterval 
 
-
-console.log(balls)
 
 
 const nextSlide = () =>{
@@ -50,8 +48,52 @@ const nextSlide = () =>{
            
        }
 
+
+}
+
+
+
+
+
+
+
+
+
+const previousSlide = () =>{
+
+       current.classList.remove("current")
+
+
+       if (current.previousElementSibling) {
+           
+            current.previousElementSibling.classList.add("current")
+
+       } else {
+
+        slides[slides.length - 1].classList.add("current")                                
+           
+       }
+
+
+
+       currentBall.classList.remove("current-ball")
+
+
+       if (currentBall.previousElementSibling) {
+           
+            currentBall.previousElementSibling.classList.add("current-ball")
+
+       } else {
+
+            balls[balls.length - 1].classList.add("current-ball")                                
+           
+       }
+
+      
+
        console.log('working')
 
 }
 
-nextSlide()
+
+previousSlide()
