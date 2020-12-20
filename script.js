a6a23d70ad35dc65dba114d08ef94363db2603c6
briansimiyuj <briansimiyuj@gmail.com>
2020-12-20 09:43:26 +0300

    Accesing the DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+const slides = document.querySelectorAll(".slides")
+
+const current = document.querySelector(".current")  
+
+const currentBall = document.querySelector(".current-ball")
+
+const prevBTN  = document.querySelector("#prev-btn")
+
+const nextBTN = document.querySelector("#next-btn")
+
+console.log(slides, current, currentBall, prevBTN, nextBTN)       
