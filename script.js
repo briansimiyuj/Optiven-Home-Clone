63cae68c2c05536f39af550bdc95510a4c6b45eb
briansimiyuj <briansimiyuj@gmail.com>
2020-12-21 07:26:50 +0300

    Temporally set auto to false

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const prevBTN  = document.querySelector("#prev-btn")
 
 const balls = document.querySelectorAll(".ball")
 
-const auto = true
+const auto = false
 
 const nextBTN = document.querySelector("#next-btn")
 
@@ -114,9 +114,7 @@ nextBTN.addEventListener("click", e =>{
      if (auto) {
         
         clearInterval()
-
-        console.log('working')
-
+        
     }
 
 })  
@@ -131,9 +129,7 @@ prevBTN.addEventListener("click", e =>{
      if (auto) {
         
         clearInterval()
-
-        console.log('working')
-
+        
     }
 
 })
